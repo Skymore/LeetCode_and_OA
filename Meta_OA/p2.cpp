9560e5c20cdbf83324af10bb5059cd665661c840
skyyymore <122345615@qq.com>
2024-03-16 09:58:36 -0700

    add problems

---
+++ Meta_OA/p2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+/*
+ * 题意：
+ * reviews是一组评论，positive_words是一组正面词，negative_words是一组负面词，intensity是一组强度词。
+ * 评论中的词可能是正面词，也可能是负面词，也可能是强度词，也可能是其他词。
+ * 如果评论中的词是正面词，那么评论的分数加1，如果评论中的词是负面词，那么评论的分数减1，如果评论中的词是强度词，那么下一个正面词或者负面词的分数变化会增加1。
+ * 求每个评论是"positive"还是"negative"还是"neutral"。
+ */
+vector<string> solution(vector<string> &reviews, vector<string> &positive_words, vector<string> &negative_words,
+             vector<string> intensity) {
+
+
+}
+int main() {
+
+}
